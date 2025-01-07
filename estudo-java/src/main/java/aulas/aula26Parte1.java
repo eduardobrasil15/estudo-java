5fbb97b977613656ad6fff29c6eeb096ac063a9e
eduardobrasil15 <56721470+eduardobrasil15@users.noreply.github.com>
2025-01-07 15:32:43 -0300

    Update aula26Parte1.java

--- estudo-java/src/main/java/aulas/aula26Parte1.java
+++ estudo-java/src/main/java/aulas/aula26Parte1.java
@@ -1,8 +1,19 @@
 package aulas;
 
+import javax.xml.transform.stream.StreamSource;
+import java.util.Scanner;
+
 public class aula26Parte1 {
 
     public static void main(String[] args) {
-        
-    }
+
+        Scanner sc = new Scanner(System.in);
+
+        String x;
+        x = sc.next();
+        System.out.println("Voce digitou: " + x);
+
+        sc.close();
+
+        }
 }
