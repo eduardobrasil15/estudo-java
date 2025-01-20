94dc98b4817295023fdafb53c607b04f24d47f4b
eduardobrasil15 <56721470+eduardobrasil15@users.noreply.github.com>
2025-01-20 20:54:24 -0300

    aula38

---
+++ estudo-java/src/main/java/aulas/aula38.java
@@ -0,0 +1,22 @@
+package aulas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aula38 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int minutos = sc.nextInt();
+
+        double conta = 50.0;
+        if (minutos > 100) {
+            conta = conta + (minutos - 100) * 2.0; /* é o mesmo q conta +=(minutos - 100) * 2.0; */
+        }
+        System.out.printf("valor da conta = R$ %.2f%n", conta);
+
+        sc.close();
+    }
+}
